b301cc6a35e6814908a81d7783e82d85f0fa53d3
Alex <hlbu@yandex.ru>
2021-02-25 20:55:12 +0300

    my_server.py++-

---
+++ client_my.py
@@ -0,0 +1,90 @@
+# Программа сервера для ОТПРАВКИ приветствия сервера и получения ответа
+from socket import *
+import json
+import time
+import click
+import requests
+import re
+
+# запросы клиента:
+quit = {
+    'action': 'quit'
+}
+AUTH_CLIENT = {
+    'action': 'authenticate',
+    # 'action': 'dfgdfg',
+    'time': time.time(),
+    'user': {
+        'account_name': 'C0deMaver1ck',
+        'password': 'CorrectHorseBatterStaple'
+    }
+}
+PRESENTS_MSG = {  # сообщение о присутствии — presence
+    'action': 'presence',
+    'time': time.time(),
+    'type': 'status',
+    'user': {
+        'account_name': 'C0deMaver1ck',
+        'status': 'Yep, I am here!'
+    }
+}
+
+# ========================client===================================================
+
+# ========================client===================================================
+BUFSIZ = 640
+ENCODE = 'utf-8'
+
+
+def current_start_client(addr, port):
+    auth_from_client_json = json.dumps(AUTH_CLIENT)
+    msg_presence_json = json.dumps(PRESENTS_MSG)
+    quit_json = json.dumps(quit)
+
+    tcpCliSock = socket(AF_INET, SOCK_STREAM)
+    tcpCliSock.connect((addr, int(port)))  # установка связи с сервером
+    while True:
+        time.sleep(3)
+        tcpCliSock.send(auth_from_client_json.encode(ENCODE))
+        print(f'Recieved auth ')
+        data = tcpCliSock.recv(BUFSIZ)  # ожидание (получение) ответа
+        if data.decode(ENCODE) == 'An optional message/notification - Ok!':
+            tcpCliSock.send(msg_presence_json.encode(ENCODE))
+            print(data.decode(ENCODE))
+
+        else:
+            qwe = data.decode(ENCODE)
+            match = re.findall(r'probe!!!', qwe)
+            if match:
+                print('probe!!!')
+                tcpCliSock.send(quit_json.encode(ENCODE))
+                break
+
+
+    tcpCliSock.close()
+
+
+# msg_full = ''
+# while True:
+#     msg_from_server = s.recv(5)
+#     if len(msg_from_server) <= 0:
+#         break
+#     msg_full += msg_from_server.decode('utf-8')
+#     print(f'Текущее время: {msg_from_server.decode('utf-8')}')
+#     # print( msg_from_server.decode('utf-8'))
+# print(msg_full)
+#
+# s.close()
+
+
+# ==========click============
+
+@click.command()
+@click.argument('addr')
+@click.argument('port')
+def main(addr, port):
+    current_start_client(addr, port)
+
+
+if __name__ == '__main__':
+    main()
---
+++ server_my.py
@@ -0,0 +1,83 @@
+from contextlib import closing
+from socket import *
+import json
+import requests
+import time
+import click
+
+# Ответы сервераa
+LIST_AUTH = [
+    {
+        "response": 200,
+        "alert": "An optional message/notification - Ok!"
+    },
+    {
+        "response": 402,
+        "error": "This could be 'wrong password' or 'no account with that name'"
+    },
+]
+PROBE = {
+    "action": "probe!!!",
+    "time": time.time(),
+}
+
+# =====================server=======================================
+# =====================server=======================================
+BUFSIZ = 640
+ENCODE = 'utf-8'
+
+
+def current_start_server(addr, port):
+    lst_answers_after_auth_json = json.dumps(LIST_AUTH)
+    PROBE_json = json.dumps(PROBE)
+
+    tcpSerSock = socket(AF_INET, SOCK_STREAM)  # создаем сокет сервера
+    tcpSerSock.bind((addr, int(port)))  # связываем сокет с адресом И ПОРТОМ
+    tcpSerSock.listen(5)  # клиентов 5
+    print('Server in listening..........')
+
+    while True:  # бесконечный цикл сервера
+        print('Waiting for client...')
+        tcpCliSock, addr = tcpSerSock.accept()  # ждем клиента, при соединении .accept()
+        print(f'Connected from: {addr}')
+        while True:  # цикл связи
+            data = tcpCliSock.recv(BUFSIZ)  # принимает данные от клиента
+            data_dict = json.loads(data.decode(ENCODE))
+            auth_response_server_list = json.loads(lst_answers_after_auth_json)
+            if not data:
+                break  # разрываем связь если данных нет
+            if data_dict['action'] == 'authenticate':
+                for var_response in auth_response_server_list:
+                    if var_response['response'] == 200:
+                        msg = var_response['alert']
+                        tcpCliSock.send(bytes(msg, ENCODE))
+
+            elif data_dict['action'] == 'presence':
+                msg = PROBE_json.encode(ENCODE)
+                tcpCliSock.send(msg)
+                print('прилетел presence')
+            elif data_dict['action'] == 'quit':
+                tcpCliSock.send('finish'.encode(ENCODE))
+                print('прилетел quit')
+            elif data_dict['action'] != 'authenticate':
+                msg = auth_response_server_list[1]['error']
+                tcpCliSock.send(bytes('You sent me != authenticate" {}"'.format(msg), 'utf8'))
+                print('ошибка auth')
+
+        tcpCliSock.close()  # закрываем сеанс (сокет) с клиентом
+
+
+        tcpSerSock.close()  # закрытие сокета сервера
+
+
+
+# ==========click============
+@click.command()
+@click.argument('addr')
+@click.argument('port')
+def main(addr, port):
+    current_start_server(addr, port)
+
+
+if __name__ == '__main__':
+    main()
